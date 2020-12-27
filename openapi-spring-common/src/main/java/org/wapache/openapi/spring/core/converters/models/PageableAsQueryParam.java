daac9d044572069e9f7d9eb4ce331dd296ef2a00
ykuang <ykuang@wapache.org>
2020-12-27 20:42:32 +0800

    修改sort参数的说明

--- openapi-spring-common/src/main/java/org/wapache/openapi/spring/core/converters/models/PageableAsQueryParam.java
+++ openapi-spring-common/src/main/java/org/wapache/openapi/spring/core/converters/models/PageableAsQueryParam.java
@@ -45,18 +45,25 @@ import org.wapache.openapi.v3.annotations.media.Schema;
  */
 @Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-@Parameter(in = ParameterIn.QUERY
-		, name = "page"
-		, schema = @Schema(type = "integer", title="第几页", description = "取值范围:(0..N), 第一页传0", defaultValue = "0"))
-@Parameter(in = ParameterIn.QUERY
-		, name = "size"
-		, schema = @Schema(type = "integer", title="每页记录数", description = "", defaultValue = "20"))
-@Parameter(in = ParameterIn.QUERY
-		, name = "sort"
-		, array = @ArraySchema(schema = @Schema(type = "string", title="排序规则"
-	, description = "格式: 属性名[,asc|desc]"
-	+ "默认的排序方式是asc升序"
-	+ "支持多个排序规则, 譬如 http://localhost/xxx?sort=a,desc&sort=b,asc")))
+@Parameter(
+	in = ParameterIn.QUERY
+	, name = "page"
+	, schema = @Schema(type = "integer", title="第几页", description = "取值范围:(0..N), 第一页传0", defaultValue = "0")
+)
+@Parameter(
+	in = ParameterIn.QUERY
+	, name = "size"
+	, schema = @Schema(type = "integer", title="每页记录数", description = "", defaultValue = "20")
+)
+@Parameter(
+	in = ParameterIn.QUERY
+	, name = "sort"
+	, array = @ArraySchema(schema = @Schema(type = "string", title="排序规则"
+		, description = "参数值的格式: `属性名[,asc|desc]`, "
+		+ "默认的排序方式是asc升序, "
+		+ "支持多个排序规则, 譬如 `http://localhost/xxx?sort=a,desc&sort=b,asc`"
+	))
+)
 public @interface PageableAsQueryParam {
 
 }
